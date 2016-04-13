2b1f90ec63f539cd0fbbb6152bb4d383b0f5c360
Israel Canul <israel.canul@gmail.com>
2016-04-13 16:23:04 -0500

    ultimas modificaciones

--- core/src/com/mygdx/game/Entitys/MyActor.java
+++ core/src/com/mygdx/game/Entitys/MyActor.java
@@ -7,8 +7,7 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
-import static com.badlogic.gdx.Gdx.files;
-
+import static com.mygdx.game.Constants.unitScale;
 /**
  * Created by icanul on 4/5/16.
  */
@@ -23,6 +22,12 @@ public class MyActor extends Actor {
     public void draw (Batch batch, float parentAlpha) {
         Color color = getColor();
         batch.setColor(color.r, color.g, color.b, color.a * parentAlpha);
-        batch.draw(region,1,4*16,16,16);
+        batch.draw(region,convertir(0),convertir(4),unitScale,unitScale);
     }
+
+    public float convertir(Integer valor){
+        return unitScale * valor;
+    }
+
+
 }
--- core/src/com/mygdx/game/Screens/MainGameScreen.java
+++ core/src/com/mygdx/game/Screens/MainGameScreen.java
@@ -9,36 +9,48 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.mygdx.game.Constants;
 import com.mygdx.game.Entitys.MyActor;
+
+import static com.mygdx.game.Constants.unitScale;
 
 
 /**
  * Created by icanul on 4/5/16.
  */
 public class MainGameScreen extends BaseScreen {
+    private final World world;
+    private Box2DDebugRenderer debugRenderer;
     private Stage stage;
     TiledMap map;
     OrthogonalTiledMapRenderer renderer;
     OrthographicCamera camera;
-    float unitScale = 16;
+
     private TiledMapTileLayer suelo;
 
     public MainGameScreen(Game game) {
         super(game);
+        //se carga el mapa tile
         map = new TmxMapLoader().load("mimundo.tmx");
         suelo = (TiledMapTileLayer)map.getLayers().get("coliciones");
         MapObjects objects = map.getLayers().get("colicion").getObjects();
         System.out.print(objects.getCount());
 
+        // se crea el mundo de box2d
+        world = new World(new Vector2(0, -10), true);
+        debugRenderer= new Box2DDebugRenderer();
 
+
+        //se crea la camara de tile
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800 / unitScale, 480 / unitScale);
-
         renderer = new OrthogonalTiledMapRenderer(map, 1 / unitScale );
-
 
     }
 
@@ -48,7 +60,6 @@ public class MainGameScreen extends BaseScreen {
         stage=new Stage(new ExtendViewport(800, 480));
 
         MyActor actor=new MyActor();
-        //actor.setSize(16f,16f);
         stage.addActor(actor);
 
         renderer.getBatch();
@@ -58,8 +69,8 @@ public class MainGameScreen extends BaseScreen {
     @Override
     public void hide() {
         stage.dispose();
-
         map.dispose();
+        world.dispose();
     }
 
     @Override
@@ -67,16 +78,17 @@ public class MainGameScreen extends BaseScreen {
         Gdx.gl20.glClearColor(0, 0, 0.5f, 1);
         Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        //se renderiza el mapa
+        world.step(1 / 60f, 6, 2);
+
+
+
         camera.update();
         renderer.setView(camera);
         renderer.render();
 
         stage.act();
-       /* stage.getBatch().begin();*/
-        //stage.getBatch().disableBlending();
         stage.draw();
-        //stage.getBatch().enableBlending();
-        /*stage.getBatch().end();*/
+
+        debugRenderer.render(world, camera.combined);
     }
 }
